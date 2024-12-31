8d317c7097fc6ea1ef1fd480d5252fcada0f220f
Aziz Bek <azizbek@Azizs-MacBook-Air-250.local>
2024-12-30 19:01:31 -0500

    1- project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "You are in the Sign up page",
+    });
+}
+
+export const login = async (req, res) => {
+    res.json({
+        data: "You are in the Login page",
+    });
+}
+
+export const logout = async (req, res) => {
+    res.json({
+        data: "You are in the Logout page",
+    });
+}
